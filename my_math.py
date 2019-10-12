d1886c169f4941d908242a6335193f71b6201e7a
{sanket1203} <{patelsanket2003@gmail.com}>
2019-10-12 18:03:29 +0530

    Initial commit

---
+++ my_math.py
@@ -0,0 +1,10 @@
+
+
+def add(a,b):
+    return a+b
+
+def mult(a,b):
+    return a*b
+
+def dot(a,b):
+    return a[0]*b[0]+a[1]*b[1]
---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+setup(name='datapeek',
+      version='0.1',
+      description='A simple library for dealing with raw data',
+      url='https://github.com/sanket1203/datapeek_py',
+      author='Sanket Patel',
+      author_email='sanket.patel@wissen.com',
+      license='MIT',
+      packages=['datapeek'],
+      install_requires=[
+                'fuzzywuzzy',
+                'pandas'
+      ],
+      include_package_data=True,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
